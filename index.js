6a7c1067ea3222535cc2654417af1216ab0fbf20
George Hyland <gohylander@gmail.com>
2023-04-01 23:56:39 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+    if (someNumber < 400)
+    return 'This one is on me!';
+
+    else if (someNumber < 2000)
+    return "That will be twenty bucks.";
+
+    else if (someNumber <= 2001)
+    return 'I will gladly take your thirty bucks.';
+
+    else (someNumber > 2500)
+    return 'No can do.';
+}    
+
+
+
+
+function ternaryCheckCity(city){
+ if (city === 'NYC')
+ return 'Ok, sounds good.';
+
+ else if (city !== 'NYC')
+ return 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous')
+  return 'Thank you so much.';
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  else if (tip === 'not as generous')
+  return 'Thank you.';
+
+  else (tip === "anything else")
+  return 'Bye.';
 }
